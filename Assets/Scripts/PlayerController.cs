3f90a4db50617ae01d132a820e47f6495966d973
Bartek <kieronsky94@outlook.com>
2023-01-15 03:25:22 +0100

    animations
    to fix: player controller collider has strange transform when using a/d keys

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,41 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
 
     public float moveSpeed;
-    //public Rigidbody rb;
+
     public float jumpForce;
     public CharacterController controller;
 
     private Vector3 moveDirection;
     public float gravityScale;
-    
 
-    
+    public Animator anim;
+    public Transform pivot;
+    public float rotateSpeed;
+
+    public GameObject playerModel;
+
+
+
     void Start()
     {
-        //rb = GetComponent<Rigidbody>();
-        controller= GetComponent<CharacterController>();
+
+        controller = GetComponent<CharacterController>();
+
     }
 
-    
+
     void Update()
     {
-        /*rb.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, rb.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
+        float yStore = moveDirection.y;
 
-        if (Input.GetButtonDown("Jump"))
-        {
-            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
-        }*/
-
-        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
+        moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+        moveDirection = moveDirection.normalized * moveSpeed;
+        moveDirection.y = yStore;
 
         if (controller.isGrounded)
         {
-            
+            moveDirection.y = 0f;
             if (Input.GetButtonDown("Jump"))
             {
                 moveDirection.y = jumpForce;
@@ -44,5 +46,17 @@ public class PlayerController : MonoBehaviour
 
         moveDirection.y += (Physics.gravity.y * gravityScale * Time.deltaTime);
         controller.Move(moveDirection * Time.deltaTime);
+
+        //Move player based on camera look direction
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, pivot.rotation.eulerAngles.y, 0f);
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
+        }
+
+
+        anim.SetBool("isGrounded", controller.isGrounded);
+        anim.SetFloat("Speed", (Mathf.Abs(Input.GetAxis("Vertical")) + Mathf.Abs(Input.GetAxis("Horizontal"))));
     }
 }
